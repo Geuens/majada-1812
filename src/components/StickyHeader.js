b7fe0327229b840c6cf683e12b4b68f033dd9d3a
Geuens <cgeuens719@gmail.com>
2025-06-07 17:34:48 +0200

    cambio en stickyheader

--- src/components/StickyHeader.js
+++ src/components/StickyHeader.js
@@ -1,4 +1,4 @@
-// src/components/StickyHeader.js
+/// src/components/StickyHeader.js
 import React, { useEffect, useRef, useState } from 'react';
 import Link from 'next/link';
 
@@ -28,34 +28,34 @@ const StickyHeader = () => {
   }, [isFixed]);
 
   return (
-    <header
-      className={`app-header ${isFixed ? 'header-fixed' : ''}`}
-      style={{ paddingTop: isFixed ? `${navHeight}px` : '0' }}
-    >
-      <div className="header-inner">
+    <div className="App" style={{ paddingTop: isFixed ? `${navHeight}px` : '0' }}>
+      <header className={`app-header ${isFixed ? 'header-fixed' : ''}`}>
         <div
-          className="header-titles"
-          style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}
+          className="header-inner"
         >
-          <h1 className={`wsj-title ${isFixed ? 'title-fixed' : ''}`}>majada 1812</h1>
-          <h2 className={`wsj-sub-title ${isFixed ? 'sub-title-hidden' : ''}`}>
-            Ad Virtutem, Ad Libertas
-          </h2>
-        </div>
+          <div
+            className="header-titles"
+            style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}
+          >
+            <h1 className={`wsj-title ${isFixed ? 'title-fixed' : ''}`}>majada 1812</h1>
+            <h2 className={`wsj-sub-title ${isFixed ? 'sub-title-hidden' : ''}`}>
+              Ad Virtutem, Ad Libertas
+            </h2>
+          </div>
 
-        <nav ref={navRef} className={isFixed ? 'fixed' : ''}>
-          <Link href="/values">Noticias</Link>
-          <Link href="/finance">Opinión</Link>
-          <Link href="/data">Cultura/Deporte</Link>
-        </nav>
+          <nav ref={navRef} className={isFixed ? 'fixed' : ''}>
+            <Link href="/values">Noticias</Link>
+            <Link href="/finance">Opinión</Link>
+            <Link href="/data">Cultura/Deporte</Link>
+          </nav>
 
-        <div className="chat-link">
-          <Link href="/chat">@Contacto</Link>
+          <div className="chat-link">
+            <Link href="/chat">@Contacto</Link>
+          </div>
         </div>
-      </div>
-    </header>
+      </header>
+    </div>
   );
 };
 
 export default StickyHeader;
-
