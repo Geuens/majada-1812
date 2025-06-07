8f2713e33bf4fd75e488d7467f41f60634127edf
Geuens <cgeuens719@gmail.com>
2025-06-07 17:22:36 +0200

    cambio css

--- src/pages/chat.js
+++ src/pages/chat.js
@@ -1,8 +1,6 @@
 import React, { useEffect, useRef, useState } from 'react';
 import Link from 'next/link';
 import Chat from '../components/pages_not_pages/Chat'; // Ajusta la ruta si hace falta
-import '../styles/global.css';
-
 
 export async function getStaticProps() {
   const baseUrl = process.env.NEXT_PUBLIC_BASE_URL;
